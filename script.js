d0203bd5ec18f2f1049e7fd898ecf82c22c7792d
seu-username <seu-email>
2024-02-10 03:27:22 -0300

    Sortear Número

---
+++ script.js
@@ -0,0 +1,17 @@
+function generateNumber() {
+    const min=Math.ceil(document.querySelector('.input-min').value)
+    const max= Math.floor(document.querySelector('.input-max').value)
+    const value = document.querySelector(".result")
+
+
+if (max > min) {
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
+
+    value.innerHTML = (result)
+
+} else {
+    alert("O valor mínimo tem que ser MENOR que o valor máximo")
+}
+}
+
+    
